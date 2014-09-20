1b2010b53f43fe1f6ba6deabeae44d517e3849ff
anna-tolchikova <any.tolchikova@gmail.com>
2014-09-20 16:23:46 +0300

    cleanup

--- forlabs/out/artifacts/forlabs_war_exploded/js/scripts.js
+++
@@ -1,8 +0,0 @@
-function setHiddenBeBY(){
-    document.all.localeStrI.value="be_BY";
-    document.LocaleForm.submit();
-}
-function setHiddenEnUs(){
-    document.all.localeStrI.value="en_US";
-    document.LocaleForm.submit();
-}
